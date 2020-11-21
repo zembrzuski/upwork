f87b0c2693d66dc4c618713cbcf1f6f5d3b1c07f
zem <rodrigo.zembrzuski@gruporbs.com.br>
2020-11-21 12:08:30 -0300

    update task ok

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ import task_management_client
 import update_client
 
 # TODO throttling, slicing, etc.
+# TODO write about possible timezone issues.
 
 # data_ingestion.init_database()
-task_id = reindex_client.execute()
-task_management_client.check_task_status(task_id)
-update_client.execute()
+reindex_task_id = reindex_client.execute()
+task_management_client.check_task_status(reindex_task_id)
+
+update_task_id = update_client.execute()
+task_management_client.check_task_status(update_task_id)
+
+print('finished')
